834e69a3b48148969758a1a2fd9c5cdc088fc689
Shashankvp000 <shashankvp.000@gmail.com>
2020-07-02 22:41:40 +0530

    Todo-Application-part1-Java

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'shashank', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'shashank', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'shashank', 'Learn Microservices', sysdate(), false);
